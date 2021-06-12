f601a0c1b153fa77688a0e9f6de89a4aa4dd8abc
YiFan-Evan <61977364+YiFan-Evan@users.noreply.github.com>
2021-06-12 10:14:32 +0800

    changemode

--- app/src/main/java/com/example/mytest/user/Mode.kt
+++ app/src/main/java/com/example/mytest/user/Mode.kt
@@ -4,7 +4,7 @@ object Mode {
     const val LOCAL = 1
     const val REMOTE = 2
 
-    val type = Mode.LOCAL //在这里设置本地存储/远程连接
+    val type = Mode.REMOTE //在这里设置本地存储/远程连接
 
     fun getModeType() :Int = when (Mode.type) {
         Mode.LOCAL -> {
