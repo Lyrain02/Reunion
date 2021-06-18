31a953a1d9b7e28da4909281e798b064be8e6460
YiFan-Evan <61977364+YiFan-Evan@users.noreply.github.com>
2021-06-18 10:22:33 +0800

    update

---
+++ app/src/main/java/com/example/mytest/Api.java
@@ -0,0 +1,57 @@
+package com.example.mytest;
+
+import android.util.Pair;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.example.mytest.Util.*;
+
+public class Api {
+
+    public static Map<String, String> get_user_id(String username){
+        Map<String, String> map = new HashMap<>();
+        map.put("username",username);
+        Pair<String, Integer> send = send(map2json(map), "/user/id");
+        if(send.second==404){
+            return null;
+        }
+        return json2map(send.first);
+    }
+
+    public static int sign_up(String username,String password){
+        Map<String, String> map = new HashMap<>();
+        map.put("username",username);
+        map.put("password",password);
+        return send(map2json(map),"/login/sign-up").second;
+    }
+
+    public static int sign_in(String username,String password){
+        Map<String, String> map = new HashMap<>();
+        map.put("username",username);
+        map.put("password",password);
+        return send(map2json(map),"/login/sign-in").second;
+    }
+
+
+    public static int sign_out(String username,String password){
+        Map<String, String> map = new HashMap<>();
+        map.put("username",username);
+        map.put("password",password);
+        return send(map2json(map),"/login/sign-out").second;
+    }
+
+    public static Map<String, String> get_user(int id){
+        Map<String, String> map = new HashMap<>();
+        map.put("id",Integer.toString(id));
+        Pair<String, Integer> send = send(map2json(map), "/user/detail");
+        if(send.second==404){
+            return null;
+        }
+        return json2map(send.first);
+    }
+
+//    public static int set_image(String src){
+//        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+//    }
+}
--- app/src/main/java/com/example/mytest/utils/Remote.kt
+++ app/src/main/java/com/example/mytest/utils/Remote.kt
@@ -1,9 +1,11 @@
 package com.example.mytest.utils
 
+import com.example.mytest.Api
 import com.example.mytest.R
 import com.example.mytest.user.Data
 import com.example.mytest.user.Person
 import com.example.mytest.user.PersonClue
+import com.example.mytest.user.User
 
 /*----注释-----*/
 //远程服务器数据访问
@@ -23,35 +25,92 @@ object Remote {
     }
 
     fun getUserStatusRemote(uid:Int):String{
+        var res=""
+        val t=Thread {
+            val user = Api.get_user(uid)
+            println(user)
+            if(user==null){
+                res=""
+            }else{
+                if(!user.containsKey("identify")||user.get("identify")==""){
+                    res= User.AUTH_UNFINISHED
+                }else{
+                    res= User.AUTH_FINISHED
+                }
+            }
+        }
+        t.start()
+        t.join()
         //连接数据库
         //通过用户id，获取用户状态
-        return Data.userList[uid].status
+        return res
     }
 
     fun insertUserRemote(username: String,pw:String):Int{
+        var res =""
+        val t=Thread(Runnable {
+            val signUp = Api.sign_up(username, pw)
+            println(signUp)
+            if(signUp==404){
+                res=""
+            }else{
+                res= Api.get_user_id(username)["id"].toString()
+            }
+        })
+        t.start()
+        t.join()
+        println(Integer.parseInt(res))
         //更新数据库
         //将用户命和密码插入数据库,获取用户id
         //-1表示失败，0及以上为真实用户
-        return 0
+        return if(res=="") -1 else Integer.parseInt(res)
     }
 
     fun isUserValidRemote(username: String):Boolean{
+        var signIn =0
+        val t=Thread(Runnable {
+            signIn = Api.sign_in(username, "xxxxxx")
+        })
+        t.start()
+        t.join()
+        if(signIn==404){
+            return false
+        }
         //在数据库中查询用户名
         //若存在，返回true；否则，返回false
         return true
     }
 
     fun isPasswordValidRemote(name:String,pwd:String):Boolean{
+        var signIn =0
+        val t=Thread(Runnable {
+            signIn = Api.sign_in(name, pwd)
+        })
+        t.start()
+        t.join()
+        if(signIn!=200){
+            return false
+        }
         //在数据库中查询用户名和密码是否匹配
         //若匹配，返回true；否则，返回false
-
         return true
     }
 
     fun getUserIdRemote(name:String):Int{
+        var id=""
+        val t=Thread(Runnable {
+            val signIn = Api.get_user_id(name)
+            if (signIn == null){
+                id=""
+            }else{
+                id= signIn["id"].toString()
+            }
+        })
+        t.start()
+        t.join()
         //在数据库中，通过用户名，查询用户id
         //-1表示失败，0为测试用户，1以上为真实用户
-        return 0
+        return if(id=="") -1 else Integer.parseInt(id)
     }
 
     fun updateUserImageRemote(uid:Int, img:Int):Boolean{
